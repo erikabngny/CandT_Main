90066463f808768455d8f14f0fcd6ee42b0c76a5
erika-bonganay <erika.bonganay@codeandtheory>
2020-07-03 13:14:55 +0800

    First Commit Configuration

---
+++ locators/page_elements.py
@@ -0,0 +1,13 @@
+""" PYTEST PAGE ELEMENT IDENTIFIERS GO HERE """
+
+from selenium.webdriver.common.by import By
+
+
+class LoginPageLocators(object):
+    USN_INPUT = (By.XPATH, 'sample_xpath')
+    PWD_INPUT = (By.XPATH, 'sample_xpath')
+    LOGIN_CTA = (By.XPATH, 'sample_xpath')
+
+
+class LandingPageLocators(object):
+    SEARCH_CTA = (By.XPATH, 'sample_xpath')
---
+++ tests/test_new_project_login.py
@@ -0,0 +1,11 @@
+import pytest, time
+from utils import environment as env
+from utils.environment import Pages as on
+
+
+@pytest.mark.usefixtures("test_setup")
+class TestProjectLogin(object):
+    def test_successful_login(self):
+        self.driver.get(env.page_url)
+
+        on.LoginPage.login_as_standard_user(self)
